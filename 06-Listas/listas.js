55bb5946d3f92c037ce8722d18b46b6145feb1d4
jaubass <jaubass@gmail.com>
2023-03-31 13:56:51 +0200

    Ejercicio 7 conjuntos - Set

--- 06-Listas/listas.js
+++ 06-Listas/listas.js
@@ -22,12 +22,16 @@ console.log("=================================================================")
 console.log("Una lista de tus 3 películas favoritas (objetos con propiedades: titulo, director, fecha)")
 const favoriteFilms = [
     {title: 'Pulp Fiction', director: 'Tarantino', year: 1986/05/05},
-    {title: 'Lord of the rings', director: 'Jackson', year: 2010/01/01},
+    {title: 'Lord of the rings', director: 'Jackson', year: 2012/01/01},
     {title: 'ET', director: 'Spilberg', year: 1990/12/12}
 ];
 console.log("=================================================================")
-
 console.log("Una nueva lista que contenga los directores de la lista de películas original (utilizando map)");
+const directorList = favoriteFilms.map(valor => {
+    return valor.director.concat.title
+})
+
+
 // Recorrer lista con foreach
 favoriteFilms.forEach(v => {
     console.log(v)
@@ -40,6 +44,14 @@ console.log(newArray)
 
 console.log("=================================================================")
 console.log("Una nueva lista que contenga los títulos de la lista de películas original (utilizando map)");
-
 let tituloPeli = favoriteFilms.map(title => title.tituloPeli)
 console.log(tituloPeli)
+
+function titulosFunc({ title }) {
+    return `${title}`;
+  }
+
+console.log("=================================================================")
+console.log("Una nueva lista que contenga las películas posteriores al 1 de enero de 2010");
+const filmAnt = favoriteFilms.filter(obj => obj.year < 2010/01/01) 
+console.log(filmAnt);
---
+++ 07-Sets/conjuntos.js
@@ -0,0 +1,43 @@
+/* 
+Crea un archivo llamado conjuntos.js que contenga las siguientes líneas
+- Un nuevo Set con los nombres de tu familia
+- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
+*/
+
+const array = [
+    {nombre: 'Jaco', edad: 46},
+    {nombre: 'Marcus', edad: 62},
+
+];
+const miFamily = new Set(array)
+console.log(miFamily)
+
+// .add()
+console.log("========Añadir un valor en el Set========")
+miFamily.add('Jau')
+console.log(miFamily)
+
+// El Set solo muestra valores únicos
+miFamily.add('Jau')
+console.log(miFamily)
+miFamily.add('Javascript')
+console.log(miFamily)
+
+// .delete()
+console.log("========Borrar un valor en el Set========")
+miFamily.delete("Jau")
+console.log(miFamily)
+
+// .has()
+console.log("========Buscar un valor en el Set========")
+console.log(miFamily.has(46))
+
+// .size
+console.log("========Buscar el tamaño del Set========")
+console.log(miFamily.size)
+
+// iterar Sets
+miFamily.forEach(valor => {
+    console.log(valor)
+})
